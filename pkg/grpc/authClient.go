70ebcdead9a702ee7e96dcfbb60748a03d028bb5
johnnyKahiu <wawerukahiu@gmail.com>
2026-02-17 00:36:07 +0300

    removed auth tokens

--- pkg/grpc/authClient.go
+++ pkg/grpc/authClient.go
@@ -2,7 +2,6 @@ package grpc
 
 import (
 	"context"
-	"log"
 
 	pb "github.com/JohnnyKahiu/speed_sales_proto/user"
 	"google.golang.org/grpc"
@@ -38,7 +37,7 @@ func (s *AuthService) ValidateUserToken(ctx context.Context, token string) (stri
 		return "", false
 	}
 
-	log.Printf("resp = %v,\n", resp.Rights)
+	// log.Printf("resp = %v,\n", resp.Rights)
 	// fmt.Printf("token = %v\n", token)
 
 	if !resp.Valid {
